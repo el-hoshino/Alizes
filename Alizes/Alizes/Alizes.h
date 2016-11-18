c9283f1959eea3628d6f450c1c05228bb18ab5b6
史 翔新 <shi_xiangxin@mages.co.jp>
2016-11-18 13:21:33 +0900

    Initial project and license

---
+++ Alizes/Alizes/Alizes.h
@@ -0,0 +1,19 @@
+//
+//  Alizes.h
+//  Alizes
+//
+//  Created by 史　翔新 on 2016/11/18.
+//  Copyright © 2016年 net.crazism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Alizes.
+FOUNDATION_EXPORT double AlizesVersionNumber;
+
+//! Project version string for Alizes.
+FOUNDATION_EXPORT const unsigned char AlizesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alizes/PublicHeader.h>
+
+
