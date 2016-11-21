698518841987160074fcc0ef084d17e71a552c1c
史 翔新 <shi_xiangxin@mages.co.jp>
2016-11-21 12:56:26 +0900

    Seperate MorseCodeDictionary from MorseCode

---
+++ Alizes/Alizes/MorseCodeDictionary.swift
@@ -0,0 +1,65 @@
+//
+//  MorseCodeDictionary.swift
+//  Alizes
+//
+//  Created by 史　翔新 on 2016/11/21.
+//  Copyright © 2016年 net.crazism. All rights reserved.
+//
+
+import Foundation
+
+public struct MorseCodeDictionary {
+	
+	let alphabetDictionary: [String: String] = [
+		"A": ".-",
+		"B": "-...",
+		"C": "-.-.",
+		"D": "-..",
+		"E": ".",
+		"F": "..-.",
+		"G": "--.",
+		
+		"H": "....",
+		"I": "..",
+		"J": ".---",
+		"K": "-.-",
+		"L": ".-..",
+		"M": "--",
+		"N": "-.",
+		
+		"O": "---",
+		"P": ".--.",
+		"Q": "--.-",
+		"R": ".-.",
+		"S": "...",
+		"T": "-",
+		
+		"U": "..-",
+		"V": "...-",
+		"W": ".--",
+		"X": "-..-",
+		"Y": "-.--",
+		"Z": "--..",
+	]
+	
+	let numberDictionary: [String: String] = [
+		"0": "-----",
+		"1": ".----",
+		"2": "..---",
+		"3": "...--",
+		"4": "....-",
+		"5": ".....",
+		"6": "-....",
+		"7": "--...",
+		"8": "---..",
+		"9": "----.",
+	]
+	
+	public func getCode(forCharacter character: Character) -> String? {
+		
+		let text = String(character).uppercased()
+		return self.alphabetDictionary[text] ?? self.numberDictionary[text]
+		
+	}
+	
+}
