d6b03b315bfc0d6fd6f1cb680db3cd45ca69be6c
史 翔新 <x_shi@yumemi.co.jp>
2017-12-22 15:03:15 +0900

    Playground contents update

--- Alizes/Playground.playground/Contents.swift
+++ Alizes/Playground.playground/Contents.swift
@@ -3,10 +3,17 @@
 import UIKit
 import Alizes
 
-let str = "Hello playground"
+let str = "Hello, World!"
 
 let morse = MorseCode(str)
 let baudot = BaudotCode(str)
 
 print(morse)
+print(morse.initializedString)
+print(morse.binaryCodeContainer)
+
+print()
+
 print(baudot)
+print(baudot.binaryCodeContainer)
+print(baudot.initializedString)
